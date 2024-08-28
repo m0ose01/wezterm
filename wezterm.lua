eb1f13cb7cf9413c8fa3ae657dd66a3abff347db
Seamus <m0ose01@proton.me>
2024-08-28 17:08:43 +0100

    Change theme to rose-pine.

--- wezterm.lua
+++ wezterm.lua
@@ -1,9 +1,9 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.color_scheme = 'OneDark (base16)'
+config.color_scheme = 'rose-pine'
 config.font = wezterm.font 'Inconsolata Nerd Font Mono'
 config.font_size = 14
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 config.hide_tab_bar_if_only_one_tab = true
 return config
