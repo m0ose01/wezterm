b1c87767211613659f29612bd3f817ad66f22c53
Séamus O'Sullivan <seamus.osullivan@crick.ac.uk>
2025-10-22 13:23:41 +0100

    Map Ctrl-Backspace to Ctrl-w.

--- wezterm.lua
+++ wezterm.lua
@@ -6,4 +6,17 @@ config.font = wezterm.font 'Inconsolata Nerd Font Mono'
 config.font_size = 14
 config.window_background_opacity = 0.8
 config.hide_tab_bar_if_only_one_tab = true
+
+config.keys = {
+	{
+		key = "Backspace",
+		mods = "CTRL",
+		action = wezterm.action.SendKey {
+			key = "w",
+			mods = "CTRL",
+		}
+	}
+}
+
+
 return config
